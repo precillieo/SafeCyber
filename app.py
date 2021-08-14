a4cd147b6641e30a068c5d6d3809fbd749c64330
Precillieo <kolawoleprecious99@gmail.com>
2021-08-14 02:37:09 +0100

    Updates for deployment

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ from datetime import datetime
 
 app= Flask(__name__, template_folder= "templates")
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://egeyxqvvwyzdxm:1eef9bc22e70b9f307273cf3279f874b61e88d76fa2cb7530b57c936b2543f76@ec2-3-233-100-43.compute-1.amazonaws.com:5432/d2npo0hg6mu43l'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db= SQLAlchemy(app)
 
 
 
 class User(db.Model):
 	id= db.Column(db.Integer, primary_key=True)
-	first_name= db.Column(db.String(20), unique=True, nullable=False)
-	last_name= db.Column(db.String(20), unique=True, nullable=False)
+	first_name= db.Column(db.String(20), nullable=False)
+	last_name= db.Column(db.String(20), nullable=False)
 	email_address= db.Column(db.String(120), unique=True, nullable=False)
 	phone_number= db.Column(db.String(20), nullable=False)
 	social_media_handle= db.Column(db.String(20), nullable=False)
@@ -23,6 +24,7 @@ class User(db.Model):
 	date_posted= db.Column(db.DateTime, nullable=False, default= datetime.utcnow)
 	image_file= db.Column(db.String(20), nullable=False, default= 'default.jpg')
 	category_of_harassment= db.Column(db.String(60), nullable=False, default= 'CyberStalking')
+	db.create_all()
 
 	def __repr__(self):
 		return f"User('{self.first_name}', '{self.email_address}', '{self.date_posted}', '{self.category_of_harassment}')"
@@ -37,7 +39,6 @@ def home():
 @app.route('/details', methods=['GET', 'POST'])
 def details():
 	if request.method == 'POST':
-		db.create_all()
 		user= User(first_name= request.form.get('fn'), last_name=request.form.get('ln'),
 			email_address=request.form.get('email'), phone_number= request.form.get('phone'),
 			social_media_handle=request.form.get('handle'), location=request.form.get('location'),
@@ -45,6 +46,7 @@ def details():
 			image_file= request.files.get('imagefile'), category_of_harassment= request.form.get('category_of_harassment'))
 		db.session.add(user)
 		db.session.commit()
+		print('Successfully added')
 
 	return render_template('form.html')
 
